c4ff2484e4e75bcc5a6b4dbe3941ba54514a4d9c
Sean Davey <sdavey@arizona.edu>
2023-02-03 15:46:31 -0700

    initial commit

---
+++ variables.tf
@@ -0,0 +1,29 @@
+
+variable "AWS" {
+  type = object({
+    instance_type = string,
+    region        = string,
+    public_key    = string
+  })
+}
+
+variable "dadi_cli" {
+  type = object({
+    run        = bool,
+    parameters = string,
+  })
+}
+
+variable "workqueue_factory" {
+  type = object({
+    run = bool
+  })
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "workqueue_password" {
+  type = string
+}
